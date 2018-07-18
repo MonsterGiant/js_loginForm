5329306956a6683cbafd35b80674c2985a9a2008
MonsterGiant <41343128+MonsterGiant@users.noreply.github.com>
2018-07-18 14:22:47 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 'use strict'
 
+var attempt = 3;
 
-
-/*var attempt = 3;
-
-var users = XMLHttpRequest
 function validate(){
     var username = document.getElementById("user").value;
     var password = document.getElementById("pass").value;
-    if ( username == 1){
+    if ( username == data.users[1] && password == data.users[1]){
         alert ("Login successfully");
-        if (username == base.user2.login){
+        if (username == data.users[1]){
             alert("Logged as admin! Be carefull!!!")
         }
         attempt = 3;
@@ -26,14 +23,14 @@ function validate(){
             return false;
         }
     }
-}*/
+};
 
 var data;
 var request = new XMLHttpRequest();
 request.open('GET', 'scripts/base.json', true);
 
 request.onload = function () {
-    if (request.status >= 200 && request.status < 400){
+    if (request.status >= 200 && request.status < 400) {
         data = JSON.parse(request.responseText);
         console.log(data.users[1]);
     }
@@ -41,3 +38,4 @@ request.onload = function () {
 
 request.send();
 
+console.log(data);
